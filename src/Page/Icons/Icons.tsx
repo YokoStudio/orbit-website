3a20f568c2f9cda9b410f0ae5c0e5cbff83a9f2d
eanlami <eanlami@outlook.com>
2024-09-16 11:43:59 +0330

    fix svg render (cpu bug), added color to ShapeIcon

--- src/Page/Icons/Icons.tsx
+++ src/Page/Icons/Icons.tsx
@@ -15,6 +15,7 @@ const Icons: React.FC = () => {
     const [folders, setFolders] = useState<string[]>(['Interface', 'Weather']); 
     const [isFilterVisible, setIsFilterVisible] = useState<boolean>(false);
     const [activeTab, setActiveTab] = useState<string>('Shape'); 
+    const [iconColor, setIconColor] = useState<string>('#FF0000');
 
     const handleSearch = (term: string) => {
         setSearchTerm(term);
@@ -78,6 +79,7 @@ const Icons: React.FC = () => {
                         searchTerm={searchTerm}
                         switchChecked={switchChecked}
                         selectedFolders={selectedFolders}
+                        iconColor={iconColor}
                     />
                     )}
 
--- src/component/ShapeIcon/ShapeIcon.tsx
+++ src/component/ShapeIcon/ShapeIcon.tsx
@@ -1,126 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import './ShapeIcon.scss';
-// import axios from 'axios';
-// import { XMLParser } from 'fast-xml-parser';
-// import loadingG from '../../assets/loading.gif';
-// import '../../base/type-style.scss';
-
-// interface ShapeIconProps {
-//   searchTerm: string;
-//   switchChecked: boolean;
-//   selectedFolders: string[];
-// }
-
-// const ShapeIcon: React.FC<ShapeIconProps> = ({ searchTerm, switchChecked, selectedFolders }) => {
-//   const [icons, setIcons] = useState<{ name: string; path: string }[]>([]);
-//   const [loading, setLoading] = useState<boolean>(false);
-//   const [error, setError] = useState<string | null>(null);
-//   const [svgContent, setSvgContent] = useState<{ [key: string]: string }>({});
-
-
-//   useEffect(() => {
-//     const fetchIcons = async () => {
-//       try {
-//         const response = await axios.get('https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir?prefix=icons/shape&max-keys=1000');
-//         const parser = new XMLParser();
-//         const jsonData = parser.parse(response.data);
-//         const ShapeIcon = jsonData.ListBucketResult.Contents.map((item: any) => ({
-//           name: item.Key.split('/').pop(),
-//           path: item.Key,
-//         }));
-//         setIcons(ShapeIcon);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error('Error fetching icons:', error);
-//         setError('Error receiving icons');
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchIcons();
-//   }, []);
-
-//   // دریافت محتوای SVG به عنوان متن
-//   const fetchSvgContent = async (url: string, iconName: string) => {
-//     try {
-//       const response = await axios.get(url);
-//       setSvgContent((prev) => ({
-//         ...prev,
-//         [iconName]: response.data,
-//       }));
-//     } catch (error) {
-//       console.error('Error fetching SVG content:', error);
-//     }
-//   };
-
-//   const filteredIcons = icons
-//     .filter((icon) => icon.name.toLowerCase().includes(searchTerm.toLowerCase()))
-
-//     .filter(icon => {
-//         const iconType = switchChecked ? 'fill' : 'outline';
-//         return icon.path.includes(iconType);
-//     })
-                
-//     .filter((icon) => {
-//       const iconFolder = icon.path.split('/')[2];
-//       return selectedFolders.length === 0 || selectedFolders.includes(iconFolder);
-//     });
-
-//   return (
-//     <div className="icon-shape-body">
-//       {loading ? (
-//         <div className="loading">
-//           <img src={loadingG} alt="logo" width="128px" height="128px" />
-//           <span>Loading...</span>
-//         </div>
-//       ) : error ? (
-//         <p>{error}</p>
-//       ) : (
-//         <div className="icon-list">
-//           {filteredIcons.length > 0 ? (
-//             filteredIcons.map((icon, index) => {
-//               const imageUrl = `https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir/${icon.path}`;
-
-//               // اگر محتوای SVG هنوز بارگذاری نشده بود، آن را بارگذاری کنید
-//               if (!svgContent[icon.name]) {
-//                 fetchSvgContent(imageUrl, icon.name);
-//               }
-
-//               return (
-//                 <div className="item" key={index}>
-//                   {svgContent[icon.name] ? (
-//                     <div className='icon-wrapper'>
-//                         <div
-//                             className="svg-shape-icon"
-//                             dangerouslySetInnerHTML={{ __html: svgContent[icon.name] }}
-//                             onClick={() => {
-//                             const link = document.createElement('a');
-//                             link.href = imageUrl;
-//                             link.download = icon.name;
-//                             link.click();
-//                         }}
-//                     />
-//                     </div>
-//                   ) : (
-//                     <span className='skelton'>Loading SVG...</span>
-//                   )}
-//                   <span className='b2'>{icon.name.replace('.svg', '')}</span>
-//                 </div>
-//               );
-//             })
-//           ) : (
-//             <div>
-//               <span >Icon not found</span>
-//             </div>
-//           )}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default ShapeIcon;
-
 import React, { useEffect, useState } from 'react';
 import './ShapeIcon.scss';
 import axios from 'axios';
@@ -132,19 +9,23 @@ interface ShapeIconProps {
   searchTerm: string;
   switchChecked: boolean;
   selectedFolders: string[];
+  iconColor: string;
 }
 
-const ShapeIcon: React.FC<ShapeIconProps> = ({ searchTerm, switchChecked, selectedFolders }) => {
+const ShapeIcon: React.FC<ShapeIconProps> = ({ searchTerm, switchChecked, selectedFolders, iconColor = '#ffffff' }) => {
   const [icons, setIcons] = useState<{ name: string; path: string }[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [svgContent, setSvgContent] = useState<{ [key: string]: string }>({});
   const [filteredIcons, setFilteredIcons] = useState<{ name: string; path: string }[]>([]);
 
+  // Fetch icons from the server
   useEffect(() => {
     const fetchIcons = async () => {
       try {
-        const response = await axios.get('https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir?prefix=icons/shape&max-keys=1000');
+        const response = await axios.get(
+          'https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir?prefix=icons/shape&max-keys=1000'
+        );
         const parser = new XMLParser();
         const jsonData = parser.parse(response.data);
         const fetchedIcons = jsonData.ListBucketResult.Contents.map((item: any) => ({
@@ -161,36 +42,49 @@ const ShapeIcon: React.FC<ShapeIconProps> = ({ searchTerm, switchChecked, select
     };
 
     fetchIcons();
-  }, []); // این قسمت فقط یک بار هنگام بارگذاری کامپوننت اجرا می‌شود
+  }, []);
 
+  // Filter icons based on search term, type (fill/outline), and selected folders
   useEffect(() => {
-    // فیلتر آیکون‌ها براساس جستجو، نوع (fill/outline)، و فولدرهای انتخابی
     const filtered = icons
       .filter((icon) => icon.name.toLowerCase().includes(searchTerm.toLowerCase()))
-      .filter(icon => {
-        const iconType = switchChecked ? 'fill' : 'outline'; // فیلتر نوع (پر یا خط)
+      .filter((icon) => {
+        const iconType = switchChecked ? 'fill' : 'outline';
         return icon.path.includes(iconType);
       })
       .filter((icon) => {
-        const iconFolder = icon.path.split('/')[2]; // فیلتر بر اساس فولدر انتخابی
+        const iconFolder = icon.path.split('/')[2];
         return selectedFolders.length === 0 || selectedFolders.includes(iconFolder);
       });
 
     setFilteredIcons(filtered);
-  }, [icons, searchTerm, switchChecked, selectedFolders]); // به روزرسانی فیلتر آیکون‌ها در زمان تغییر props
+  }, [icons, searchTerm, switchChecked, selectedFolders]);
 
-  // دریافت محتوای SVG به عنوان متن
-  const fetchSvgContent = async (url: string, iconName: string) => {
-    try {
-      const response = await axios.get(url);
-      setSvgContent((prev) => ({
-        ...prev,
-        [iconName]: response.data,
-      }));
-    } catch (error) {
-      console.error('Error fetching SVG content:', error);
+  // Fetch SVG content for all filtered icons
+  useEffect(() => {
+    const fetchAllSvgContent = async () => {
+      for (const icon of filteredIcons) {
+        const imageUrl = `https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir/${icon.path}`;
+        try {
+          const response = await axios.get(imageUrl);
+          const svgData = response.data;
+          const coloredSvg = svgData
+            .replace(/fill="[^"]*"/g, `fill="${iconColor}"`)
+            .replace(/stroke="[^"]*"/g, `stroke="${iconColor}"`);
+          setSvgContent((prev) => ({
+            ...prev,
+            [icon.name]: coloredSvg,
+          }));
+        } catch (error) {
+          console.error('Error fetching SVG content:', error);
+        }
+      }
+    };
+
+    if (filteredIcons.length > 0) {
+      fetchAllSvgContent();
     }
-  };
+  }, [filteredIcons, iconColor]);
 
   return (
     <div className="icon-shape-body">
@@ -204,36 +98,27 @@ const ShapeIcon: React.FC<ShapeIconProps> = ({ searchTerm, switchChecked, select
       ) : (
         <div className="icon-list">
           {filteredIcons.length > 0 ? (
-            filteredIcons.map((icon, index) => {
-              const imageUrl = `https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir/${icon.path}`;
-
-              // اگر محتوای SVG هنوز بارگذاری نشده بود، آن را بارگذاری کنید
-              if (!svgContent[icon.name]) {
-                fetchSvgContent(imageUrl, icon.name);
-              }
-
-              return (
-                <div className="item" key={icon.path}> {/* key از مسیر یونیک فایل */}
-                  {svgContent[icon.name] ? (
-                    <div className='icon-wrapper'>
-                        <div
-                            className="svg-shape-icon"
-                            dangerouslySetInnerHTML={{ __html: svgContent[icon.name] }}
-                            onClick={() => {
-                            const link = document.createElement('a');
-                            link.href = imageUrl;
-                            link.download = icon.name;
-                            link.click();
-                        }}
+            filteredIcons.map((icon) => (
+              <div className="item" key={icon.path}>
+                {svgContent[icon.name] ? (
+                  <div className="icon-wrapper">
+                    <div
+                      className="svg-shape-icon"
+                      dangerouslySetInnerHTML={{ __html: svgContent[icon.name] }}
+                      onClick={() => {
+                        const link = document.createElement('a');
+                        link.href = `https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir/${icon.path}`;
+                        link.download = icon.name;
+                        link.click();
+                      }}
                     />
-                    </div>
-                  ) : (
-                    <span className='skelton'></span>
-                  )}
-                  <span className='b2'>{icon.name.replace('.svg', '')}</span>
-                </div>
-              );
-            })
+                  </div>
+                ) : (
+                  <span className="skelton">Loading SVG...</span>
+                )}
+                <span className="b2">{icon.name.replace('.svg', '')}</span>
+              </div>
+            ))
           ) : (
             <div>
               <span>Icon not found</span>
