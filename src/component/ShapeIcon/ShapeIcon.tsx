d26b7109df35d8c1a6286296565b0b3312562d7a
eanlami <eanlami@outlook.com>
2024-09-30 19:31:39 +0330

    fixed similarNames search

--- src/component/ShapeIcon/ShapeIcon.tsx
+++ src/component/ShapeIcon/ShapeIcon.tsx
@@ -1,3 +1,167 @@
+// import React, { useEffect, useState } from 'react';
+// import './ShapeIcon.scss';
+// import axios from 'axios';
+// import { XMLParser } from 'fast-xml-parser';
+// import loadingG from '../../assets/loading.gif';
+// import '../../base/type-style.scss';
+// import Fuse from 'fuse.js';
+
+// interface ShapeIconProps {
+//   searchTerm: string;
+//   switchChecked: boolean;
+//   selectedFolders: string[];
+//   iconColor: string
+// }
+
+// const ShapeIcon: React.FC<ShapeIconProps> = ({
+//   searchTerm,
+//   switchChecked,
+//   selectedFolders,
+//   iconColor,
+// }) => {
+//   const [icons, setIcons] = useState<{ name: string; path: string }[]>([]);
+//   const [loading, setLoading] = useState<boolean>(true);
+//   const [error, setError] = useState<string | null>(null);
+//   const [svgContent, setSvgContent] = useState<{ [key: string]: string }>({});
+//   const [filteredIcons, setFilteredIcons] = useState<{ name: string; path: string }[]>([]);
+
+//   useEffect(() => {
+//     const fetchIcons = async () => {
+//       try {
+//         const response = await axios.get('https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir?prefix=icons/shape&max-keys=1000');
+//         const parser = new XMLParser();
+//         const jsonData = parser.parse(response.data);
+//         const fetchedIcons = jsonData.ListBucketResult.Contents.map((item: any) => ({
+//           name: item.Key.split('/').pop(),
+//           path: item.Key,
+//         }));
+//         setIcons(fetchedIcons);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error('Error fetching icons:', error);
+//         setError('Error receiving icons');
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchIcons();
+//   }, []);
+
+//   useEffect(() => {
+//     const fuse = new Fuse(icons, {
+//       // isCaseSensitive: false,
+//       // includeScore: false,
+//       // shouldSort: true,
+//       // includeMatches: false,
+//       // findAllMatches: false,
+//       // minMatchCharLength: 1,
+//       // location: 0,
+//       threshold: 0.3,
+//       // distance: 100,
+//       // useExtendedSearch: false,
+//       // ignoreLocation: false,
+//       // ignoreFieldNorm: false,
+//       // fieldNormWeight: 1,
+//       keys: ['name']
+//     });
+//     const result = fuse.search(searchTerm);
+//     console.log('Filtered icons:', result);
+//     const iconsToBeFiltered = searchTerm 
+//       ? result.map(({item: {name, path}}) => ({ name, path  }))
+//       : icons
+
+//     const filtered = iconsToBeFiltered
+//       // .filter((icon) => icon.name.toLowerCase().includes(searchTerm.toLowerCase()))
+//       .filter((icon) => {
+//         const iconType = switchChecked ? 'fill' : 'outline';
+//         return icon.path.includes(iconType);
+//       })
+//       .filter((icon) => {
+//         const iconFolder = icon.path.split('/')[2];
+//         return selectedFolders.length === 0 || selectedFolders.includes(iconFolder);
+//       });
+
+//     setFilteredIcons(filtered);
+//   }, [icons, searchTerm, switchChecked, selectedFolders]);
+
+//   const fetchSvgContent = async (url: string, iconName: string) => {
+//     try {
+//       const response = await axios.get(url);
+//       const svgData = response.data;
+//       const coloredSvg = svgData
+//         .replace(/fill="[^"]*"/g, `fill="${iconColor}"`)
+//         .replace(/stroke="[^"]*"/g, `fill="${iconColor}"`);
+//       setSvgContent((prev) => ({
+//         ...prev,
+//         [iconName]: coloredSvg,
+//       }));
+//     } catch (error) {
+//       console.error('Error fetching SVG content:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     // فراخوانی مجدد محتوای SVG برای هر آیکون فیلتر شده
+//     filteredIcons.forEach((icon) => {
+//       const imageUrl = `https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir/${icon.path}`;
+//       fetchSvgContent(imageUrl, icon.name); // واکشی هر آیکون و به‌روزرسانی محتوای SVG
+//     });
+//   }, [filteredIcons, iconColor]); // هر بار که لیست آیکون‌های فیلتر شده یا رنگ تغییر کرد، اجرا شود
+
+//   const handleDownload = (iconName: string, svgData: string) => {
+//     const blob = new Blob([svgData], { type: 'image/svg+xml' });
+//     const url = URL.createObjectURL(blob);
+//     const link = document.createElement('a');
+//     link.href = url;
+//     link.download = iconName;
+//     link.click();
+//     URL.revokeObjectURL(url);
+//   };
+
+//   return (
+//     <div className="icon-shape-body">
+//       {loading ? (
+//         <div className="loading">
+//           <img src={loadingG} alt="logo" width="128px" height="128px" />
+//           <span>Loading...</span>
+//         </div>
+//       ) : error ? (
+//         <p>{error}</p>
+//       ) : (
+//         <div className="shape-icon-list">
+//           {filteredIcons.length > 0 ? (
+//             filteredIcons.map((icon) => {
+//               const svgData = svgContent[icon.name];
+//               return (
+//                 <div className="item" key={icon.path}>
+//                   {svgData ? (
+//                     <div className="icon-wrapper">
+//                       <div
+//                         className="svg-shape-icon"
+//                         dangerouslySetInnerHTML={{ __html: svgData }}
+//                         onClick={() => handleDownload(icon.name, svgData)}
+//                       />
+//                     </div>
+//                   ) : (
+//                     <span className="skelton">Loading SVG...</span>
+//                   )}
+//                   <span className="b1 icon-name">{icon.name.replace('.svg', '')}</span>
+//                 </div>
+//               );
+//             })
+//           ) : (
+//             <div>
+//               <span>Icon not found</span>
+//             </div>
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ShapeIcon;
+
 import React, { useEffect, useState } from 'react';
 import './ShapeIcon.scss';
 import axios from 'axios';
@@ -10,7 +174,13 @@ interface ShapeIconProps {
   searchTerm: string;
   switchChecked: boolean;
   selectedFolders: string[];
-  iconColor: string
+  iconColor: string;
+}
+
+interface Icon {
+  name: string;
+  path: string;
+  similarNames?: string[]; // نام‌های مشابه از JSON
 }
 
 const ShapeIcon: React.FC<ShapeIconProps> = ({
@@ -19,27 +189,43 @@ const ShapeIcon: React.FC<ShapeIconProps> = ({
   selectedFolders,
   iconColor,
 }) => {
-  const [icons, setIcons] = useState<{ name: string; path: string }[]>([]);
+  const [icons, setIcons] = useState<Icon[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [svgContent, setSvgContent] = useState<{ [key: string]: string }>({});
-  const [filteredIcons, setFilteredIcons] = useState<{ name: string; path: string }[]>([]);
+  const [filteredIcons, setFilteredIcons] = useState<Icon[]>([]);
 
   useEffect(() => {
     const fetchIcons = async () => {
       try {
-        const response = await axios.get('https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir?prefix=icons/shape&max-keys=1000');
+        // واکشی آیکون‌ها از S3
+        const response = await axios.get(
+          'https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir?prefix=icons/shape&max-keys=1000'
+        );
         const parser = new XMLParser();
         const jsonData = parser.parse(response.data);
         const fetchedIcons = jsonData.ListBucketResult.Contents.map((item: any) => ({
           name: item.Key.split('/').pop(),
           path: item.Key,
         }));
-        setIcons(fetchedIcons);
+
+        // واکشی JSON برای نام‌های مشابه
+        const jsonResponse = await axios.get(`https://orbit-website.s3.ir-thr-at1.arvanstorage.ir/IconList.json?t=${Date.now()}`);
+
+        const similarNamesData = jsonResponse.data;
+
+        // ترکیب اطلاعات JSON با آیکون‌ها
+        const iconsWithSimilarNames = fetchedIcons.map((icon: Icon) => {
+          const similarNamesEntry = similarNamesData[icon.name.replace('.svg', '')];
+          const similarNames = similarNamesEntry ? similarNamesEntry : [];
+          return { ...icon, similarNames };
+        });
+
+        setIcons(iconsWithSimilarNames);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching icons:', error);
-        setError('Error receiving icons');
+        console.error('Error fetching icons or JSON data:', error);
+        setError('Error receiving icons or JSON data');
         setLoading(false);
       }
     };
@@ -49,29 +235,18 @@ const ShapeIcon: React.FC<ShapeIconProps> = ({
 
   useEffect(() => {
     const fuse = new Fuse(icons, {
-      // isCaseSensitive: false,
-      // includeScore: false,
-      // shouldSort: true,
-      // includeMatches: false,
-      // findAllMatches: false,
-      // minMatchCharLength: 1,
-      // location: 0,
       threshold: 0.3,
-      // distance: 100,
-      // useExtendedSearch: false,
-      // ignoreLocation: false,
-      // ignoreFieldNorm: false,
-      // fieldNormWeight: 1,
-      keys: ['name']
+      includeMatches: true,
+      keys: ['name', 'similarNames'], // جستجو در نام و نام‌های مشابه
     });
     const result = fuse.search(searchTerm);
-    console.log('Filtered icons:', result);
-    const iconsToBeFiltered = searchTerm 
-      ? result.map(({item: {name, path}}) => ({ name, path  }))
-      : icons
+    const iconsToBeFiltered = searchTerm
+      ? result.map(({ item }) => item)
+      : icons;
+
+      console.log(result);
 
     const filtered = iconsToBeFiltered
-      // .filter((icon) => icon.name.toLowerCase().includes(searchTerm.toLowerCase()))
       .filter((icon) => {
         const iconType = switchChecked ? 'fill' : 'outline';
         return icon.path.includes(iconType);
@@ -100,13 +275,19 @@ const ShapeIcon: React.FC<ShapeIconProps> = ({
     }
   };
 
+  const formatIconName = (name: string) => {
+    return name
+      .replace('.svg', '')          // حذف پسوند .svg
+      .replace(/-\d+$/, '')        // حذف عدد از انتهای نام (مثلاً -2 یا -3)
+      .replace(/-/g, ' ');          // جایگزینی - با فاصله
+  };
+
   useEffect(() => {
-    // فراخوانی مجدد محتوای SVG برای هر آیکون فیلتر شده
     filteredIcons.forEach((icon) => {
       const imageUrl = `https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir/${icon.path}`;
-      fetchSvgContent(imageUrl, icon.name); // واکشی هر آیکون و به‌روزرسانی محتوای SVG
+      fetchSvgContent(imageUrl, icon.name);
     });
-  }, [filteredIcons, iconColor]); // هر بار که لیست آیکون‌های فیلتر شده یا رنگ تغییر کرد، اجرا شود
+  }, [filteredIcons, iconColor]);
 
   const handleDownload = (iconName: string, svgData: string) => {
     const blob = new Blob([svgData], { type: 'image/svg+xml' });
@@ -145,7 +326,7 @@ const ShapeIcon: React.FC<ShapeIconProps> = ({
                   ) : (
                     <span className="skelton">Loading SVG...</span>
                   )}
-                  <span className="b1 icon-name">{icon.name.replace('.svg', '')}</span>
+                  <span className="b2 icon-name">{formatIconName(icon.name)}</span>
                 </div>
               );
             })
--- src/component/StrokeIcon/StrokeIcon.tsx
+++ src/component/StrokeIcon/StrokeIcon.tsx
@@ -1,9 +1,179 @@
+// import React, { useEffect, useState } from 'react';
+// import './StrokeIcon.scss';
+// import axios from 'axios';
+// import { XMLParser } from 'fast-xml-parser';
+// import loadingG from '../../assets/loading.gif';
+// import OrInput from '../OrInput/OrInput';
+// import Fuse from 'fuse.js';
+
+// interface StrokeIconProps {
+//   searchTerm: string;
+//   selectedFolders: string[];
+//   strokeColor: string; // رنگ لبه آیکون
+//   strokeWidth: number; // عرض لبه آیکون
+// }
+
+// interface Icon {
+//   name: string;
+//   path: string;
+//   similarNames?: string[]; // نام‌های مشابه از JSON
+// }
+
+// const StrokeIcon: React.FC<StrokeIconProps> = ({ searchTerm, selectedFolders, strokeColor, strokeWidth }) => {
+//   const [icons, setIcons] = useState<{ name: string; path: string }[]>([]);
+//   const [loading, setLoading] = useState<boolean>(true);
+//   const [error, setError] = useState<string | null>(null);
+//   const [svgContent, setSvgContent] = useState<{ [key: string]: string }>({});
+
+//   useEffect(() => {
+//     const fetchIcons = async () => {
+//       try {
+//         const response = await axios.get('https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir?prefix=icons/stroke&max-keys=1000');
+//         const parser = new XMLParser();
+//         const jsonData = parser.parse(response.data);
+//         const StrokeIcon = jsonData.ListBucketResult.Contents.map((item: any) => ({
+//           name: item.Key.split('/').pop(),
+//           path: item.Key,
+//         }));
+
+        
+//         // واکشی JSON برای نام‌های مشابه
+//         const jsonResponse = await axios.get(`https://orbit-website.s3.ir-thr-at1.arvanstorage.ir/IconList.json?t=${Date.now()}`);
+
+//         const similarNamesData = jsonResponse.data;
+
+//         // ترکیب اطلاعات JSON با آیکون‌ها
+//         const iconsWithSimilarNames = StrokeIcon.map((icon: Icon) => {
+//           const similarNamesEntry = similarNamesData[icon.name.replace('.svg', '')];
+//           const similarNames = similarNamesEntry ? similarNamesEntry : [];
+//           return { ...icon, similarNames };
+//         });
+
+
+//         setIcons(StrokeIcon);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error('Error fetching icons:', error);
+//         setError('Error receiving icons');
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchIcons();
+//   }, []);
+
+
+
+//   // دریافت محتوای SVG به عنوان متن
+//   const fetchSvgContent = async (url: string, iconName: string) => {
+//     try {
+//       const response = await axios.get(url);
+//       let svgData = response.data;
+//       // تغییر رنگ لبه‌های SVG و عرض لبه‌ها با استفاده از مقادیر انتخابی
+//       const coloredSvg = svgData
+//         .replace(/stroke="[^"]*"/g, `stroke="${strokeColor}"`)
+//         .replace(/stroke-width="[^"]*"/g, `stroke-width="${strokeWidth}px"`);
+//       setSvgContent((prev) => ({
+//         ...prev,
+//         [iconName]: coloredSvg,
+//       }));
+//     } catch (error) {
+//       console.error('Error fetching SVG content:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     // بارگذاری مجدد محتوای SVGها با تغییر رنگ و عرض لبه
+//     icons.forEach(icon => {
+//       const imageUrl = `https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir/${icon.path}`;
+//       fetchSvgContent(imageUrl, icon.name);
+//     });
+//   }, [strokeColor, strokeWidth, icons]);
+
+//   // تابع دانلود SVG با رنگ و عرض لبه
+//   const downloadSvg = (iconName: string, svgContent: string) => {
+//     const blob = new Blob([svgContent], { type: 'image/svg+xml' });
+//     const url = URL.createObjectURL(blob);
+//     const link = document.createElement('a');
+//     link.href = url;
+//     link.download = iconName;
+//     link.click();
+//     URL.revokeObjectURL(url); // برای آزادسازی حافظه
+//   };
+//   useEffect(() => {
+//     const fuse = new Fuse(icons, {
+//       threshold: 0.3,
+//       includeMatches: true,
+//       keys: ['name', 'similarNames'], // جستجو در نام و نام‌های مشابه
+//     });
+//     const result = fuse.search(searchTerm);
+//     const iconsToBeFiltered = searchTerm
+//       ? result.map(({ item }) => item)
+//       : icons;
+// })
+     
+
+
+//   const filteredIcons = icons
+//     .filter((icon) => icon.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+
+//     .filter((icon) => {
+//       const iconFolder = icon.path.split('/')[2];
+//       return selectedFolders.length === 0 || selectedFolders.includes(iconFolder);
+//     });
+
+//   return (
+//     <div className="icon-stroke-body">
+//       {loading ? (
+//         <div className="loading">
+//           <img src={loadingG} alt="logo" width="128px" height="128px" />
+//           <span>Loading...</span>
+//         </div>
+//       ) : error ? (
+//         <p>{error}</p>
+//       ) : (
+//         <div className="icon-list">
+//           {filteredIcons.length > 0 ? (
+//             filteredIcons.map((icon, index) => (
+//               <div className="item" key={index}>
+//                 {svgContent[icon.name] ? (
+//                   <div className="icon-wrapper">
+//                     <div
+//                     className="svg-container"
+//                     dangerouslySetInnerHTML={{ __html: svgContent[icon.name] }}
+//                     onClick={() => {
+//                       // دانلود SVG با رنگ و عرض لبه
+//                       downloadSvg(icon.name, svgContent[icon.name]);
+//                     }}
+//                   />
+//                   </div>
+//                 ) : (
+//                   <span className="skelton">Loading SVG...</span>
+//                 )}
+//                 <span className="b2 icon-name">{icon.name.replace('.svg', '')}</span>
+//               </div>
+//             ))
+//           ) : (
+//             <div>
+//               <span>Icon not found</span>
+//             </div>
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default StrokeIcon;
+
 import React, { useEffect, useState } from 'react';
 import './StrokeIcon.scss';
 import axios from 'axios';
 import { XMLParser } from 'fast-xml-parser';
 import loadingG from '../../assets/loading.gif';
-import OrInput from '../OrInput/OrInput'; // اطمینان حاصل کنید که این کامپوننت به درستی وارد شده است
+import OrInput from '../OrInput/OrInput';
+import Fuse from 'fuse.js';
 
 interface StrokeIconProps {
   searchTerm: string;
@@ -12,8 +182,14 @@ interface StrokeIconProps {
   strokeWidth: number; // عرض لبه آیکون
 }
 
+interface Icon {
+  name: string;
+  path: string;
+  similarNames?: string[]; // نام‌های مشابه از JSON
+}
+
 const StrokeIcon: React.FC<StrokeIconProps> = ({ searchTerm, selectedFolders, strokeColor, strokeWidth }) => {
-  const [icons, setIcons] = useState<{ name: string; path: string }[]>([]);
+  const [icons, setIcons] = useState<Icon[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [svgContent, setSvgContent] = useState<{ [key: string]: string }>({});
@@ -28,7 +204,19 @@ const StrokeIcon: React.FC<StrokeIconProps> = ({ searchTerm, selectedFolders, st
           name: item.Key.split('/').pop(),
           path: item.Key,
         }));
-        setIcons(StrokeIcon);
+
+        // واکشی JSON برای نام‌های مشابه
+        const jsonResponse = await axios.get(`https://orbit-website.s3.ir-thr-at1.arvanstorage.ir/IconList.json?t=${Date.now()}`);
+        const similarNamesData = jsonResponse.data;
+
+        // ترکیب اطلاعات JSON با آیکون‌ها
+        const iconsWithSimilarNames = StrokeIcon.map((icon: Icon) => {
+          const similarNamesEntry = similarNamesData[icon.name.replace('.svg', '')];
+          const similarNames = similarNamesEntry ? similarNamesEntry : [];
+          return { ...icon, similarNames };
+        });
+
+        setIcons(iconsWithSimilarNames); // استفاده از آیکون‌ها با نام‌های مشابه
         setLoading(false);
       } catch (error) {
         console.error('Error fetching icons:', error);
@@ -45,7 +233,6 @@ const StrokeIcon: React.FC<StrokeIconProps> = ({ searchTerm, selectedFolders, st
     try {
       const response = await axios.get(url);
       let svgData = response.data;
-      // تغییر رنگ لبه‌های SVG و عرض لبه‌ها با استفاده از مقادیر انتخابی
       const coloredSvg = svgData
         .replace(/stroke="[^"]*"/g, `stroke="${strokeColor}"`)
         .replace(/stroke-width="[^"]*"/g, `stroke-width="${strokeWidth}px"`);
@@ -58,15 +245,20 @@ const StrokeIcon: React.FC<StrokeIconProps> = ({ searchTerm, selectedFolders, st
     }
   };
 
+  const formatIconName = (name: string) => {
+    return name
+      .replace('.svg', '')          // حذف پسوند .svg
+      .replace(/-\d+$/, '')        // حذف عدد از انتهای نام (مثلاً -2 یا -3)
+      .replace(/-/g, ' ');          // جایگزینی - با فاصله
+  };
+
   useEffect(() => {
-    // بارگذاری مجدد محتوای SVGها با تغییر رنگ و عرض لبه
     icons.forEach(icon => {
       const imageUrl = `https://orbit-icon.s3.ir-thr-at1.arvanstorage.ir/${icon.path}`;
       fetchSvgContent(imageUrl, icon.name);
     });
   }, [strokeColor, strokeWidth, icons]);
 
-  // تابع دانلود SVG با رنگ و عرض لبه
   const downloadSvg = (iconName: string, svgContent: string) => {
     const blob = new Blob([svgContent], { type: 'image/svg+xml' });
     const url = URL.createObjectURL(blob);
@@ -77,12 +269,22 @@ const StrokeIcon: React.FC<StrokeIconProps> = ({ searchTerm, selectedFolders, st
     URL.revokeObjectURL(url); // برای آزادسازی حافظه
   };
 
-  const filteredIcons = icons
-    .filter((icon) => icon.name.toLowerCase().includes(searchTerm.toLowerCase()))
-    .filter((icon) => {
-      const iconFolder = icon.path.split('/')[2];
-      return selectedFolders.length === 0 || selectedFolders.includes(iconFolder);
-    });
+  // جستجو در آیکون‌ها با استفاده از Fuse.js
+  const fuse = new Fuse(icons, {
+    threshold: 0.3,
+    includeMatches: true,
+    keys: ['name', 'similarNames'], // جستجو در نام و نام‌های مشابه
+  });
+  const result = fuse.search(searchTerm);
+  const filteredIcons = searchTerm
+    ? result.map(({ item }) => item)
+    : icons;
+
+  // فیلتر کردن بر اساس پوشه‌های انتخابی
+  const finalIcons = filteredIcons.filter((icon) => {
+    const iconFolder = icon.path.split('/')[2];
+    return selectedFolders.length === 0 || selectedFolders.includes(iconFolder);
+  });
 
   return (
     <div className="icon-stroke-body">
@@ -95,24 +297,23 @@ const StrokeIcon: React.FC<StrokeIconProps> = ({ searchTerm, selectedFolders, st
         <p>{error}</p>
       ) : (
         <div className="icon-list">
-          {filteredIcons.length > 0 ? (
-            filteredIcons.map((icon, index) => (
+          {finalIcons.length > 0 ? (
+            finalIcons.map((icon, index) => (
               <div className="item" key={index}>
                 {svgContent[icon.name] ? (
                   <div className="icon-wrapper">
                     <div
-                    className="svg-container"
-                    dangerouslySetInnerHTML={{ __html: svgContent[icon.name] }}
-                    onClick={() => {
-                      // دانلود SVG با رنگ و عرض لبه
-                      downloadSvg(icon.name, svgContent[icon.name]);
-                    }}
-                  />
+                      className="svg-container"
+                      dangerouslySetInnerHTML={{ __html: svgContent[icon.name] }}
+                      onClick={() => {
+                        downloadSvg(icon.name, svgContent[icon.name]);
+                      }}
+                    />
                   </div>
                 ) : (
                   <span className="skelton">Loading SVG...</span>
                 )}
-                <span className="b2 icon-name">{icon.name.replace('.svg', '')}</span>
+                <span className="b2 icon-name">{formatIconName(icon.name)}</span>
               </div>
             ))
           ) : (
