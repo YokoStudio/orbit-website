e7b16442687df74293185231f4b8caf3d090a2d3
eanlami <eanlami@outlook.com>
2025-01-03 16:03:30 +0330

    Fixed icon c

--- src/Page/About/About.tsx
+++ src/Page/About/About.tsx
@@ -18,6 +18,13 @@ const About: React.FC = () => {
       
       {/* <p className='h6-strong'>Changelogs</p> */}
         <div className='scroll-list'>
+          <span className='h4-strong'>This is Orbit v1</span>
+          <p className='b2'>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+          </p>
+          <p className='b2'>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+          </p>
           
         </div>
      </div>
